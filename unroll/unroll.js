c2f9846e9aee4443dd495a696b465570a9481f6c
Joyce <hellojoycewong@gmail.com>
2021-07-19 16:49:43 -0700

    Write function & test for unroll();

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,24 @@
 function unroll(squareArray) {
 
+    let array = [];
+    //loop over array
+    squareArray.forEach((el, idx, arr) => {
+        //if even index, loop thry  array
+        if (idx % 2 === 0) {
+            for (let e of arr[idx]) {
+                array.push(e)
+            }
+        } //if odd idx, loop backwards
+        if (idx % 2 !== 0) {
+            for (let i = arr[idx].length - 1; i >= 0; i--) {
+                array.push(arr[idx][i])
+            }
+        }
+    });
+    return array
+
 }
+
+
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -6,4 +6,26 @@ describe("#unroll", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it('should return [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]', function () {
+    const square = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+  ];
+    expect(unroll(square)).toContain(1)
+    expect(unroll(square).length).toEqual(16)
+  })
+
+  it('should return  ["a", "b", "c", "f", "i", "h", "g", "d", "e"]', function () {
+    const smallerSquare = [
+      ["a", "b", "c"],
+      ["d", "e", "f"],
+      ["g", "h", "i"]
+    ];
+    let res = unroll(smallerSquare)
+    expect(res[3]).toEqual("f")
+    expect(res[6]).toEqual("g")
+    expect(res[8]).toEqual("i")
+  })
 });
